ef7b7b1f302d585db7021001a6e327342b1aab09
Osman Tunahan İNCİRKUŞ <106384513+osmantunahanincirkus@users.noreply.github.com>
2024-06-26 14:42:42 +0300

    Create ApplicationDBContext, add migrations and make database connection adjustments.

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7194342339a6c34ceba16c3dcd401be835cd6972")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+162a1142e7ff82fe12d12f68206d4ca269fa6d46")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
