861dc50132058ebbd0599d160eae10101d88d913
Osman Tunahan İNCİRKUŞ <106384513+osmantunahanincirkus@users.noreply.github.com>
2024-07-02 14:28:50 +0300

    Add GET method to Comment Controller.

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+727158955f3c603280f256fc73aa603aeaf18f4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+486e5720b9b5cc273f918c4622e5526c5962bb9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
