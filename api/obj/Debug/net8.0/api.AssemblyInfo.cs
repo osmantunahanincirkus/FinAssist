1b8041d0d4cf692e5a9fe3f3b77a8d48b29fefd9
Osman Tunahan İNCİRKUŞ <106384513+osmantunahanincirkus@users.noreply.github.com>
2024-07-03 15:23:40 +0300

    Add Filtering.

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3516e2211d4400ba88c4a03d4e967606d21ef71a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dd2a2230cb6b17e924be082e57681c31263d1b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
