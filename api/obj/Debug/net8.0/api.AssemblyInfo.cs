e7b55329b24a3646615fb072c8583adef543f7ef
Osman Tunahan İNCİRKUŞ <106384513+osmantunahanincirkus@users.noreply.github.com>
2024-07-04 16:02:28 +0300

    Add Token Service.

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57315e03baa997e05eeb7d96a5e9d1407c8ffad3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50c95128c102477973fcc4d1567e08930fd77b22")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
